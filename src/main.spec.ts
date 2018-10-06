20d68921a9796d0c5808a4492549c75604cbaf99
James Adarich <jamesrichford@googlemail.com>
2018-10-06 23:06:56 +0100

    add linting (#10)

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -2,9 +2,8 @@ import { Expect, Test, TestFixture } from "alsatian";
 
 @TestFixture("example test fixture")
 export class ExampleTestFixture {
-
-    @Test("doesn't throw an error")
-    public noError() {
-        Expect(() => require("./main")).not.toThrow();
-    }
+  @Test("doesn't throw an error")
+  public noError() {
+    Expect(() => require("./main")).not.toThrow();
+  }
 }
